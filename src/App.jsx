2b24c7e1588e69b77463c554e22254fbeabca9dd
yousafsabir <yousafsabir430516@gmail.com>
2022-03-11 21:26:45 +0500

    faulty backend by Nabeel

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,83 @@
 import { useState } from "react";
 import "./App.css";
 
+let empData = [];
 function App() {
   const [fName, setfName] = useState("");
   const [lName, setlName] = useState("");
-  const [Email, setEmail] = useState("");
-  const [action, setAction] = useState("");
-  const [formData, setFormData] = useState([
-    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
-    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
-    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
-  ]);
-  function whenOnchange(e) {
+  const [useremail, setEmail] = useState("");
+  const [formData, setFormData] = useState([]);
+  const [newForm, setNewForm] = useState([]);
+
+  let whenOnchange = (e) => {
     const { name, value } = e.target;
+    // console.log(name, value);
     if (name === "fName") {
       setfName(value);
     } else if (name === "lName") {
       setlName(value);
-    } else if (name === "Email") {
+    } else if (name === "email") {
       setEmail(value);
     }
-  }
-  function addData() {
-    setFormData([...formData, { fName, lName, Email }]);
-    setfName("");
-    setlName("");
-    setEmail("");
-  }
-
+  };
   return (
     <div className="App">
       <h1>Employees List</h1>
-      <form>
+      <div className="Form">
         <input
           value={fName}
-          type="text"
           onChange={(e) => {
             whenOnchange(e);
           }}
+          type="text"
           placeholder="First Name"
+          name="fName"
         />
         <input
           value={lName}
-          type="text"
           onChange={(e) => {
             whenOnchange(e);
           }}
+          type="text"
           placeholder="Last Name"
+          name="lName"
         />
         <input
-          value={Email}
-          type="email"
+          value={useremail}
           onChange={(e) => {
             whenOnchange(e);
           }}
+          type="text"
           placeholder="Email"
+          name="email"
         />
-        <button type="submit">Add Employee</button>
-      </form>
+        <button
+          onClick={() => {
+            if (fName == "" || lName == "" || useremail == "") {
+              alert("Please input All feilds");
+            } else {
+              if (useremail.includes("@") && useremail.includes(".")) {
+                setFormData([
+                  ...formData,
+                  { userfName: fName, userlName: lName, email: useremail },
+                ]);
+                setfName("");
+                setlName("");
+                setEmail("");
+              } else {
+                alert("Please enter valid email");
+              }
+            }
+          }}
+          type="add"
+        >
+          Add Employee
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
+            <th>Sr.</th>
             <th>Employee First Name</th>
             <th>Employee Last Name</th>
             <th>Employee Email Id</th>
@@ -70,14 +87,29 @@ function App() {
         <tbody>
           {formData.map((data, index) => {
             return (
-              <tr key={index}>
-                <td>{data.fName}</td>
-                <td>{data.lName}</td>
-                <td>{data.Email}</td>
+              <tr>
+                <td>{index + 1}</td>
+                <td>{data.userfName}</td>
+                <td>{data.userlName}</td>
+                <td>{data.email}</td>
                 <td>
                   <button>Update</button>
-                  <button>Delete</button>
-                  <button>View</button>
+                  <button
+                    onClick={(e) => {
+                      formData.map((newData, index) => {
+                        if (newData.email == data.email) {
+                          console.log(newData);
+                        } else {
+                          setNewForm([...newForm, newData]);
+                        }
+                      });
+                      setFormData(newForm);
+                      // console.log(newForm);
+                      // setFormData(formData);
+                    }}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             );
