5ecdd0951077aa27a990fe0c4df3656246a821ab
yousafsabir <yousafsabir430516@gmail.com>
2022-03-11 17:52:58 +0500

    created UI of the employee CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   const [lName, setlName] = useState("");
   const [Email, setEmail] = useState("");
   const [action, setAction] = useState("");
-  const [formData, setFormData] = useState([{"fName": "yousaf", "lName": "sabir", "Email": "yousafSabir2"}]);
+  const [formData, setFormData] = useState([
+    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
+    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
+    { fName: "yousaf", lName: "sabir", Email: "yousafSabir2" },
+  ]);
   function whenOnchange(e) {
     const { name, value } = e.target;
     if (name === "fName") {
@@ -17,8 +21,8 @@ function App() {
       setEmail(value);
     }
   }
-  function addData () {
-    setFormData([...formData, {fName, lName, Email}]);
+  function addData() {
+    setFormData([...formData, { fName, lName, Email }]);
     setfName("");
     setlName("");
     setEmail("");
@@ -27,42 +31,57 @@ function App() {
   return (
     <div className="App">
       <h1>Employees List</h1>
-      <form
-        action="
-    "
-      >
-        <input  value={fName} type="text" onChange={(e)=>{ whenOnchange(e)}} placeholder="First Name" />
-        <input  value={lName} type="text" onChange={(e)=>{ whenOnchange(e)}} placeholder="Last Name" />
-        <input value={Email} type="email" onChange={(e)=>{ whenOnchange(e)}} placeholder="Email" />
+      <form>
+        <input
+          value={fName}
+          type="text"
+          onChange={(e) => {
+            whenOnchange(e);
+          }}
+          placeholder="First Name"
+        />
+        <input
+          value={lName}
+          type="text"
+          onChange={(e) => {
+            whenOnchange(e);
+          }}
+          placeholder="Last Name"
+        />
+        <input
+          value={Email}
+          type="email"
+          onChange={(e) => {
+            whenOnchange(e);
+          }}
+          placeholder="Email"
+        />
         <button type="submit">Add Employee</button>
       </form>
       <table>
         <thead>
           <tr>
-            <th >Employee First Name</th>
+            <th>Employee First Name</th>
             <th>Employee Last Name</th>
             <th>Employee Email Id</th>
             <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-            {formData.map((data, index) => {
-              return (
-                <tr key={index}>
-                  <td>{data.fName}</td>
-                  <td>{data.lName}</td>
-                  <td>{data.Email}</td>
-                  <td>
-              <button>Update</button>
-              <button>Delete</button>
-              <button>View</button>
-            </td>
-                </tr>
-              );
-
-
-            })}
-            
+          {formData.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.fName}</td>
+                <td>{data.lName}</td>
+                <td>{data.Email}</td>
+                <td>
+                  <button>Update</button>
+                  <button>Delete</button>
+                  <button>View</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
