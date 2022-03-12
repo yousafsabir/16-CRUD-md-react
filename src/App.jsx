2b5b9cf85d03d7051e6b2649d68f3c2a7132809b
yousafsabir <yousafsabir430516@gmail.com>
2022-03-12 08:00:23 +0500

    changed the buttons to the icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Icon from "./assets/pen-to-square-solid.svg";
 import "./App.css";
 
 let empData = [];
@@ -93,7 +94,15 @@ function App() {
                 <td>{data.userlName}</td>
                 <td>{data.email}</td>
                 <td>
-                  <button>Update</button>
+                  <button>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="Icons"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M9 19h-4v-2h4v2zm2.946-4.036l3.107 3.105-4.112.931 1.005-4.036zm12.054-5.839l-7.898 7.996-3.202-3.202 7.898-7.995 3.202 3.201zm-6 8.92v3.955h-16v-20h7.362c4.156 0 2.638 6 2.638 6s2.313-.635 4.067-.133l1.952-1.976c-2.214-2.807-5.762-5.891-7.83-5.891h-10.189v24h20v-7.98l-2 2.025z" />
+                    </svg>
+                  </button>
                   <button
                     onClick={(e) => {
                       formData.map((newData, index) => {
@@ -108,7 +117,20 @@ function App() {
                       // setFormData(formData);
                     }}
                   >
-                    Delete
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="Icons"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      />
+                    </svg>
                   </button>
                 </td>
               </tr>
